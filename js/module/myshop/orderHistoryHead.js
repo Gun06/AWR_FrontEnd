6c2b2086c40eb7eba198d81f1c6e9d3176e7064d
Gun06 <kogun0626@naver.com>
2026-03-18 01:16:08 +0900

    chore: #43 Account 정보수정 페이지 디테일 수정

--- js/module/myshop/orderHistoryHead.js
+++ js/module/myshop/orderHistoryHead.js
@@ -4,3 +4,69 @@ $('.xans-myshop-orderhistoryhead .eDataSet').click(function () {
     $(this).siblings().removeClass('selected');
     OrderHistory.set_period_mode('search');
 });
+
+// myshop/index.html 및 기타 페이지: 상태 드롭다운·기간설정 필드가 없으면 order list에서 로드
+(function runOrderHistoryHeadFix() {
+    function removeDisplayNone(el) {
+        el.classList.remove('displaynone');
+        el.querySelectorAll('.displaynone').forEach(removeDisplayNone);
+    }
+    function fetchAndInject(orderHeadEl) {
+        var orderAcc = orderHeadEl.closest('#acc-order');
+        var isIndexPage = !!orderAcc;
+        var hasModeCs = (window.location.search || '').indexOf('mode=cs') !== -1;
+        var listUrl = '/myshop/order/list.html' + (hasModeCs ? '?mode=cs' : '');
+        fetch(listUrl)
+            .then(function (r) { return r.text(); })
+            .then(function (html) {
+                var parser = new DOMParser();
+                var doc = parser.parseFromString(html, 'text/html');
+                var srcHead = doc.querySelector('.xans-myshop-orderhistoryhead, [module="Myshop_OrderHistoryHead"]');
+                if (!srcHead) return;
+                if (isIndexPage && !hasModeCs) {
+                    var stateSelect = orderHeadEl.querySelector('.stateSelect');
+                    var srcSelect = srcHead.querySelector('.stateSelect select');
+                    if (stateSelect && srcSelect && !stateSelect.querySelector('select')) {
+                        stateSelect.appendChild(srcSelect.cloneNode(true));
+                    }
+                }
+                var dataSearch = orderHeadEl.querySelector('#dataSearch');
+                var srcDataSearch = srcHead.querySelector('#dataSearch');
+                if (dataSearch && srcDataSearch && !dataSearch.querySelector('input')) {
+                    dataSearch.innerHTML = srcDataSearch.innerHTML;
+                    var form = orderHeadEl.closest('form');
+                    if (form && isIndexPage) {
+                        form.action = location.pathname + location.search;
+                    }
+                }
+            })
+            .catch(function () {});
+    }
+    function apply() {
+        var orderHeads = document.querySelectorAll('.xans-myshop-orderhistoryhead, [module="Myshop_OrderHistoryHead"]');
+        orderHeads.forEach(function (orderHead) {
+            var orderAcc = orderHead.closest('#acc-order');
+            var stateSelect = orderHead.querySelector('.stateSelect');
+            var dataSearch = orderHead.querySelector('#dataSearch');
+            var hasModeCs = (window.location.search || '').indexOf('mode=cs') !== -1;
+            if (orderAcc) {
+                if (hasModeCs) {
+                    if (stateSelect) stateSelect.classList.add('displaynone');
+                    return;
+                }
+                if (stateSelect) {
+                    stateSelect.classList.remove('displaynone');
+                    removeDisplayNone(stateSelect);
+                }
+            }
+            var needSelect = orderAcc && !hasModeCs && stateSelect && !stateSelect.querySelector('select');
+            var needDataSearch = dataSearch && !dataSearch.querySelector('input');
+            if (needSelect || needDataSearch) fetchAndInject(orderHead);
+        });
+    }
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', apply);
+    } else {
+        apply();
+    }
+})();
